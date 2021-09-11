ba43bf1894ce33a170c1ac49258ec589ca0a44d3
sachie026 <jadhavsachin174@gmail.com>
2021-09-11 21:54:27 +0530

    Update api key variables

--- common/constants.js
+++ common/constants.js
@@ -1,7 +1,7 @@
+const KEY = "c913acc42d402fe1311885456f742e14e9a7a204915f35f8df8fa2db14ff9abf";
 exports.DAILY_ROOMS_BASE_URL = "https://api.daily.co/v1/rooms";
 exports.MY_BASE_URL = "https://sachie026.daily.co/";
-exports.API_KEY =
-  "c913acc42d402fe1311885456f742e14e9a7a204915f35f8df8fa2db14ff9abf";
+exports.API_KEY = KEY;
 exports.CONFIG = {
-  headers: { authorization: `Bearer ${API_KEY}` },
+  headers: { authorization: `Bearer ${KEY}` },
 };
