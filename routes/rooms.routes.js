95adbe4b0d682f27f3f4d50fabf6f8cceb225d5f
sachie026 <jadhavsachin174@gmail.com>
2021-09-13 11:30:49 +0530

    Update the route for participants

--- routes/rooms.routes.js
+++ routes/rooms.routes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 
 const roomController = require("../controllers/rooms.controller");
 const ROOMS_BASE = "/rooms";
+const PARTICIPANTS_BASE = "/participants";
 
 router.post(`${ROOMS_BASE}/:name`, roomController.create);
 router.get(`${ROOMS_BASE}`, roomController.findAll);
 router.get(`${ROOMS_BASE}/:id`, roomController.findById);
 router.delete(`${ROOMS_BASE}/:name`, roomController.deleteRoom);
 router.put(
-  `${ROOMS_BASE}/participants/:roomname`,
+  `${PARTICIPANTS_BASE}/:roomname`,
   roomController.updateRoomParticipants
 );
 module.exports = router;
